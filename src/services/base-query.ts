004c600d2013ab1f1fcfaf63791dd81fa5b01ed1
balinux <ryojuniyantara@gmail.com>
2025-08-04 09:17:35 +0800

    add: add .env

--- src/services/base-query.ts
+++ src/services/base-query.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
   prepareHeaders: async (headers) => {
     headers.set("Accept", "application/json");
     return headers;
